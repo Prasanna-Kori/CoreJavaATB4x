ceec9cf23327fda7532365960d37ce2e62a08fd9
Prasanna-Kori <koriprasan@gmail.com>
2023-10-10 11:34:44 +0530

    Testng

--- aug27/Lab001.java
+++ aug27/Lab001.java
@@ -3,7 +3,7 @@ public class Lab001 {
 
     public static void main(String[] args) { // public static void main - Sign - JVM ( Java virtual Machine - CPU)
         // Here I will be writing the Code
-        System.out.println("Pramod");
+        System.out.println("Prasanna");
 
     }
 
